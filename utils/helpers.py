a3ab1047257ef841d782ff4a4ca73a465fa6ca52
aka.carlll <carlfogue193@gmail.com>
2025-09-15 18:36:01 +0200

    ♻️ Add embeddings in a utils file

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,2 +1,4 @@
+from langchain.embeddings import HuggingFaceEmbeddings
 
 DOC_TYPES_DICT : dict[str, str]= {"Code":"code", "Arrétés":"arrete", "Loi":"loi", "Circulaire":"circulaire", "Autres":"autres", "Décret":"decret", "Arrets":"arret"}
+EMBEDDINGS = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
